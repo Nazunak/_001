5beadb1aad6f4113b5f83245d445450504c02d97
NazunakLaptop <work.reiste@gmail.com>
2023-08-30 15:26:19 +0300

    work wednesday cowo

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,2 +1,4 @@
-export const mult = (a, b) => a * b;
-export const sum = async(a, b) => a + b;
+// export const mult = (a, b) => a * b;
+// export const sum = async(a, b) => a + b;
+
+new Swiper('.feedback__slider');
